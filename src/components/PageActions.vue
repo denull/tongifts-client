d74ff0b9a44024fa821a99c601631d6627f89506
Denis Olshin <me@denull.ru>
2024-10-31 18:57:22 +0300

    changed locales, added anims

---
+++ src/components/PageActions.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { loc } from '../locales.js';
+defineProps({
+});
+</script>
+
+<template>
+  <section>
+    <h1>{{ loc('actionsTitle') }}</h1>
+    <h3>{{ loc('actionsSubtitle') }}</h3>
+  </section>
+</template>
+
+<style scoped>
+</style>
